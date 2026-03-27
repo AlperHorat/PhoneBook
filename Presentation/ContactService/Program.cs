98df4dcbadbb27339b0ff788201a1f76d3e45265
Alper <alperhorat@gmail.com>
2026-03-28 00:02:11 +0300

    ContactService settings and packages added

--- Presentation/ContactService/Program.cs
+++ Presentation/ContactService/Program.cs
@@ -1,17 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using PhoneBook.Infrastructure.Repositories;
+using PhoneBook.Services.Contacts;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IContactRepository, ContactRepository>();
+builder.Services.AddScoped<IContactService, ContactService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
